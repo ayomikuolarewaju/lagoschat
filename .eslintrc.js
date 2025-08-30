04e8f97b1b1c5d730d539ce2db953ccea093feb4
ayomikuolarewaju <ayomiku@ymail.com>
2025-08-30 08:48:39 +0100

    eslin

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+  },
+  ignorePatterns: ["src/generated/**"], // ignore generated
+};
